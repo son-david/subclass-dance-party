1ad4b94145eaa2f6333fea1d84adcbbaf39e1c6f
son-david <sondavid92@gmail.com>
2015-10-14 17:12:58 -0700

    implemented basicdancers

--- src/booDancers.js
+++ src/booDancers.js
@@ -1,18 +1,18 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+var booDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   this.step();
 };
 
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+booDancer.prototype = Object.create(makeDancer.prototype);
+booDancer.prototype.constructor = booDancer;
 
 // makeBlinkyDancer.prototype.oldStep = function() {
 //   makeDancer.prototype.step();
 // };
 
-makeBlinkyDancer.prototype.step = function() {
+booDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   makeDancer.prototype.step.call(this);
   // toggle() is a jQuery method to show/hide the <span> tag.
@@ -20,4 +20,3 @@ makeBlinkyDancer.prototype.step = function() {
   // other effects you can use on a jQuery-wrapped html tag.
   this.$node.toggle(this.timeBetweenSteps);
 };
-
